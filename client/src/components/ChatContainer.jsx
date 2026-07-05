2a37a28eb12c1cc0cd70b7ffc46b8241f031db6d
Md. Shamiul Haque Khan <md.shamiulhaque127@gmail.com>
2026-07-05 22:10:36 +0600

    updated sidebar

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useRef } from "react";
 import assets, { messagesDummyData } from "../assets/assets";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+  const scrollEnd = useRef();
+
+  useEffect(() => {
+    if (scrollEnd.current) {
+      scrollEnd.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
+
   return selectedUser ? (
     <div className="h-full overflow-scroll relative backdrop-blur-lg">
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
@@ -42,9 +52,42 @@ const ChatContainer = ({ selectedUser, setSelectedUser }) => {
                 {msg.text}
               </p>
             )}
-            <div></div>
+            <div className="text-xs text-center">
+              <img
+                src={
+                  msg.senderId === "680f50e4f10f3cd28382ecf9"
+                    ? assets.avatar_icon
+                    : assets.profile_martin
+                }
+                className="w-7 rounded-full"
+              />
+              <p className="text-gray-500">
+                {formatMessageTime(msg.createdAt)}
+              </p>
+            </div>
           </div>
         ))}
+
+        <div ref={scrollEnd}></div>
+      </div>
+
+      {/* bottom area */}
+      <div className="absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3">
+        <div className="flex-1 flex items-center bg-gray-100/12 px-3 rounded-full">
+          <input
+            type="text"
+            placeholder="Send a message"
+            className="flex-1 text-sm p-3 border-none rounded-lg outline-none placeholder-gray-400"
+          />
+          <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+          <label htmlFor="image">
+            <img
+              src={assets.gallery_icon}
+              className="w-5 mr-2 cursor-pointer"
+            />
+          </label>
+        </div>
+        <img src={assets.send_button} className="w-7 cursor-pointer" />
       </div>
     </div>
   ) : (
