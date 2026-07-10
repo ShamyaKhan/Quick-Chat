36887778ab3d5cb531013da5bfce2b26d14fdfac
Md. Shamiul Haque Khan <md.shamiulhaque127@gmail.com>
2026-07-10 20:19:47 +0600

    added context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
